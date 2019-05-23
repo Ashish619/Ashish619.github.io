59394b2d1e32707129918c23ff26eb927e35523f
Ashish Anand <asanand@paypal.com>
2019-05-23 16:30:40 +0530

    ffixes

--- sw.js
+++ sw.js
@@ -12,54 +12,6 @@ workbox.core.clientsClaim();
 /* injection point for manifest files.  */
 workbox.precaching.precacheAndRoute([
   {
-    "url": "icons/icon-128x128.png",
-    "revision": "79bf576c32720599dfc59fe7077945ab"
-  },
-  {
-    "url": "icons/icon-144x144.png",
-    "revision": "88f0f172de50e4fb6d4bccafedb23427"
-  },
-  {
-    "url": "icons/icon-152x152.png",
-    "revision": "3f71765b47124a940bf83df5ad4b8c9f"
-  },
-  {
-    "url": "icons/icon-192x192.png",
-    "revision": "0377f2dca4d16673c30c2e5b4aadb2d3"
-  },
-  {
-    "url": "icons/icon-256x256.png",
-    "revision": "cae5208363c21ca179da6b36eca778f4"
-  },
-  {
-    "url": "icons/icon-32x32.png",
-    "revision": "5fa7e0dc596197468e26274315825be4"
-  },
-  {
-    "url": "icons/icon-512x512.png",
-    "revision": "d0a8182ae46731e916bb1dcb5a8d9e25"
-  },
-  {
-    "url": "images/wine01.jpg",
-    "revision": "fb883bbe2aae60f4585ae33b93862916"
-  },
-  {
-    "url": "images/wine02.jpg",
-    "revision": "d0e6061e11ad74d99d86414e35f9e361"
-  },
-  {
-    "url": "images/wine03.jpg",
-    "revision": "2eba4795f43afcc6be5c13772c5d460e"
-  },
-  {
-    "url": "images/wine04.jpg",
-    "revision": "8cbe960b030290c94722f08442b23c21"
-  },
-  {
-    "url": "images/wine05.jpg",
-    "revision": "cdb476565e57e51eb8551a93741f4a67"
-  },
-  {
     "url": "index.html",
     "revision": "5bdd22f3747dfc302301c7f6963ee08d"
   },
